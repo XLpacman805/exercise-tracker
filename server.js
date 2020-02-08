81b3f6f0a6d9ac2339e84a56e7a98e6738c8f4f3
Johnny Meza <xlpacman805@gmail.com>
2020-02-08 14:48:48 -0800

    Post /new-user can read body.

--- server.js
+++ server.js
@@ -8,8 +8,15 @@ const User = require('./classes/User').User;
 let myExercise = new Exercise("my description", 10, "2020-02-08"); // test exercise; 
 let myUser = new User("Johnny", [myExercise]);
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.json({data: myUser});
+});
+
+app.post('/new-user', (req, res) => {
+    let username = req.body.username; 
+    res.json({data: username});
 });
 
 // listen for requests //process.env.PORT
