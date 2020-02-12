3e9ae33748739f5a5da1f1e37d064c748bddb00e
Johnny Meza <xlpacman805@gmail.com>
2020-02-11 21:24:10 -0800

    added Date type checking to Exercise class

--- classes/Exercise.js
+++ classes/Exercise.js
@@ -3,10 +3,10 @@ exports.Exercise = class Exercise {
      * Definition of an exercise. 
      * @param {String} description - Description of the workout.
      * @param {Number} duration - Length of workout in minutes.
-     * @param {String} date - Date in YYYY-MM-DD format.
+     * @param {Date} date - A JS Date. Should be UTC.
      */
     constructor(description, duration, date) {
-        if (typeof(description) == "string" && !isNaN(duration) && typeof(date) == "string") {
+        if (typeof(description) == "string" && !isNaN(duration) && date instanceof Date) {
             this.description = description;
             this.duration = duration;
             this.date = date;
---
+++ exercise.test.js
@@ -0,0 +1,5 @@
+const Exercise = require("./classes/Exercise").Exercise;
+
+let exercise = new Exercise("Hello world", 4, new Date());
+
+console.log(exercise);
