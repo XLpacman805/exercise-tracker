10914cf8698bda055d9cfe75784aa6ab0f21f998
Johnny Meza <xlpacman805@gmail.com>
2020-02-11 20:14:01 -0800

    removed old comment

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('/users', (req, res) => {
         });
 });
 
-// listen for requests //process.env.PORT
+// listen for requests
 const listener = app.listen(process.env.PORT, () => {
     console.log('Your app is listening on port ' + listener.address().port);
   });
